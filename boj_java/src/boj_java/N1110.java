20bf0358372c19b9a62d094c7bca15a85a0a6a19
인선 강 <sunink1@naver.com>
2021-08-02 22:07:52 +0900

    while

--- boj_java/src/boj_java/N1110.java
+++ boj_java/src/boj_java/N1110.java
@@ -7,9 +7,23 @@ public class N1110 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String n = sc.next();
-		System.out.println(n);
-
+		int n = sc.nextInt();
+		int answer = n;
+		int cnt = 0;
+		
+		while(true) {
+			
+			int f = answer/10;
+			int s = answer%10;
+			answer = s*10 + (f+s)%10;
+			cnt += 1;
+			if(answer == n) {
+				System.out.println(cnt);
+				sc.close();
+				break;
+			}
+		}
+		
 	}
 
 }
