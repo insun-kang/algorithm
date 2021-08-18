5913552a0b30f9b5eda3fd7b770f6da28372aebb
인선 강 <sunink1@naver.com>
2021-08-18 22:05:52 +0900

    Z

--- BOJ/[분할 정복][재귀]Z.py
+++ BOJ/[분할 정복][재귀]Z.py
@@ -1,32 +1,25 @@
-def Z(n, r, c):
-    global cnt
+import sys
+
+result = 0
+
+def z(n, x, y):
+    global result
+    if x == r and y == c:
+        print(int(result))
+        exit(0)
+
     if n == 1:
-        if r == 0 and c == 0:
-            pass
-        elif r == 0 and c == 1:
-            cnt += 1
-        elif r == 1 and c == 0:
-            cnt += 2
-        else:
-            cnt += 3
-    else:
-        if 0 <= r < 2**(n-1) and 0 <= c < 2**(n-1):  # 1
-            return Z(n-1, r, c)
-        elif 0 <= r < 2**(n-1) and 2**(n-1) <= c < 2**n:  # 2
-            cnt += 2**(2*(n-1))
-            return Z(n-1, r, c-(2**(n-2)))
-        elif 2**(n-1) <= r < 2**n and 0 <= c < 2**(n-1):  # 3
-            cnt += 2 * 2**(2*(n-1))
-            return Z(n-1, r-(2**(n-2)), c)
-        else:  # 4
-            cnt += 3 * 2**(2*(n-1))
-            return Z(n-1, r-(2**(n-2)), c-(2**(n-2)))
+        result += 1
+        return
+
+    if not (x <= r < x + n and y <= c < y + n):
+        result += n * n
+        return
+    z(n / 2, x, y)
+    z(n / 2, x, y + n / 2)
+    z(n / 2, x + n / 2, y)
+    z(n / 2, x + n / 2, y + n / 2)
 
 
-N, r, c = map(int, input().split())
-
-cnt = 0
-
-Z(N, r, c)
-
-print(cnt)
+n, r, c = map(int, sys.stdin.readline().split(' '))
+z(2 ** n, 0, 0)
