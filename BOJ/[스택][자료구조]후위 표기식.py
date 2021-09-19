9d18ace7d2c3b59c936f6c1b08263577d835f282
인선 강 <sunink1@naver.com>
2021-09-19 19:39:59 +0900

    후위 표기식

---
+++ BOJ/[스택][자료구조]후위 표기식.py
@@ -0,0 +1,26 @@
+ss = input()
+stack = []
+answer = ''
+
+for s in ss:
+    if s.isalpha():
+        answer += s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                answer += stack.pop()
+            stack.pop()
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                answer += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                answer += stack.pop()
+            stack.append(s)
+while stack:
+    answer += stack.pop()
+
+print(answer)
